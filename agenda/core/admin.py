74846880aae2b9b726335ca4d56ffc3e058d9fc9
Felipe Rafael Rocha <109861866+FlpRocha236@users.noreply.github.com>
2025-11-04 14:00:47 -0300

    Delete agenda directory

--- agenda/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Agenda
-
-@admin.register(Agenda)
-class AgendaAdmin(admin.ModelAdmin):
-    list_display = ("nome_completo", "email", "telefone", "observacao")
-    search_fields = ("nome_completo", "email")
